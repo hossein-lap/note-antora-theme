5f5503800d6e24c0522b97633108bcce22c43203
Hossein Esmail <hosteam01@gmail.com>
2025-12-18 17:41:00 +0330

    testing: clean footer and remove Info

---
+++ src/partials/gen-badges.pl
@@ -0,0 +1,193 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+my %icons = (
+    nix => {
+      icon  => "󱄅",
+      color => "#5277c3",
+    },
+    nixos => {
+      icon  => "󱄅",
+      color => "#7ebae4",
+    },
+    tmux => {
+      icon  => "",
+      color => "#1bb91f",
+    },
+    neovim => {
+      icon  => "",
+      color => "#57a143",
+    },
+    qemu => {
+      icon  => "",
+      color => "#ff6600",
+    },
+    kvm => {
+      icon  => "󰍹",
+      color => "#cc0000",
+    },
+    terraform => {
+      icon  => "",
+      color => "#7b42bc",
+    },
+    helm => {
+      icon  => "󱃾",
+      color => "#0f1689",
+    },
+    prometheus => {
+      icon  => "󰋩",
+      color => "#e6522c",
+    },
+    mysql => {
+      icon  => "",
+      color => "#00758f",
+    },
+    postgresql => {
+      icon  => "",
+      color => "#336791",
+    },
+    redis => {
+      icon  => "",
+      color => "#d82c20",
+    },
+    nginx => {
+      icon  => "",
+      color => "#009639",
+    },
+    apache => {
+      icon  => "",
+      color => "#d22128",
+    },
+    github => {
+      icon  => "",
+      color => "#f8f8f8",
+    },
+    gitlab => {
+      icon  => "",
+      color => "#fc6d26",
+    },
+    aws => {
+      icon  => "",
+      color => "#ff9900",
+    },
+    gcp => {
+      icon  => "󰊭",
+      color => "#4285f4",
+    },
+    nodejs => {
+      icon  => "",
+      color => "#3c873a",
+    },
+    ruby => {
+      icon  => "",
+      color => "#cc342d",
+    },
+    c => {
+      icon  => "",
+      color => "#00599c",
+    },
+    cpp => {
+      icon  => "",
+      color => "#004482",
+    },
+    zsh => {
+      icon  => "",
+      color => "#f15a24",
+    },
+    fish => {
+      icon  => "󰈺",
+      color => "#4aa3c1",
+    },
+    ubuntu => {
+      icon  => "",
+      color => "#e95420",
+    },
+    arch => {
+      icon  => "",
+      color => "#1793d1",
+    },
+    mint => {
+      icon  => "",
+      color => "#87cf3e",
+    },
+    void => {
+      icon  => "",
+      color => "#478061",
+    },
+    bash => {
+      icon => "",
+      color => '#fbfbfb',
+    },
+    freebsd => {
+      icon => "",
+      color => "#f8f8f8",
+    },
+    ansible => {
+      icon => "",
+      color => "#f8f8f8",
+    },
+    linux => {
+      icon => "",
+      color => "#f8f8f8",
+    },
+    docker => {
+      icon => "",
+      color => '#1662ed',
+    },
+    elixir => {
+      icon => "",
+      color => '#8d71a1',
+    },
+    git => {
+      icon => "",
+      color => '#fe7600',
+    },
+    golang => {
+      icon => "󰟓",
+      color => '#00acd7',
+    },
+    grafana => {
+      icon => "",
+      color => '#f7ac21',
+    },
+    k8s => {
+      icon => "",
+      color => '#2d6be5',
+    },
+    lua => {
+      icon => "",
+      color => '#000080',
+    },
+    makefile => {
+      icon => "",
+      color => '#fbfbfb',
+    },
+    perl => {
+      icon => "",
+      color => '#4c5c85',
+    },
+    vim => {
+      icon => "",
+      color => '#00982f',
+    },
+    python => {
+      icon  => "",
+      color => "#3776ab",
+    },
+);
+
+
+my $ui_root = '{{{uiRootPath}}}';
+my $img_dir = "$ui_root/img/badges";
+my $width   = 30;
+
+print qq{  <p class="badges">\n};
+
+for my $name (sort keys %icons) {
+    my $alt = ucfirst($name) . " Logo";
+    print qq{    <img src="$img_dir/badge-$name.png" alt="$alt" width="$width">\n};
+}
+
+print qq{  </p>\n};
